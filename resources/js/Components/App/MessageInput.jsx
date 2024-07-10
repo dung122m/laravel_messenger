ee4a0b49b7a382b4968af7b0770b2c814f718bf1
dungtran.9 <dung.tran@vietlightgroup.com>
2024-07-10 23:47:36 +0700

    add emoji

--- resources/js/Components/App/MessageInput.jsx
+++ resources/js/Components/App/MessageInput.jsx
@@ -1,89 +1,136 @@
-import {useState} from "react";
-import {PaperClipIcon, PhotoIcon, FaceSmileIcon, HandThumbUpIcon, PaperAirplaneIcon} from "@heroicons/react/24/solid";
+import {useState, Fragment} from "react";
+import {
+    PaperClipIcon,
+    PhotoIcon,
+    FaceSmileIcon,
+    HandThumbUpIcon,
+    PaperAirplaneIcon,
+    HeartIcon
+} from "@heroicons/react/24/solid";
 import NewMessageInput from "./NewMessageInput";
+import EmojiPicker from "emoji-picker-react";
+import {Popover, Transition} from '@headlessui/react'
 
 const MessageInput = ({conversation = null}) => {
     const [newMessage, setNewMessage] = useState("");
     const [inputErrorMessages, setInputErrorMessages] = useState("");
     const [messageSending, setMessageSending] = useState(false);
 
-
-    const onSendClick = () => {
+    const onLikeClick = () => {
         if (messageSending) {
             return;
         }
-        if (newMessage.trim() === "") {
-            setInputErrorMessages("Please enter a message or attach a file");
-            setTimeout(() => {
-                setInputErrorMessages("");
-            }, 3000);
-            return;
-        }
-
-        const formData = new FormData();
-        formData.append("message", newMessage);
+        const data = {message: "👍"};
         if (conversation.is_user) {
-            formData.append("receiver_id", conversation.id);
+            data["receiver_id"] = conversation.id;
         } else if (conversation.is_group) {
-            formData.append("group_id", conversation.id);
+            data["group_id"] = conversation.id;
         }
+        axios.post(route("message.store"), data);
+    }
+        const onHeartClick = () => {
+            if (messageSending) {
+                return;
+            }
+            const data = {message: "😍"};
+            if (conversation.is_user) {
+                data["receiver_id"] = conversation.id;
+            } else if (conversation.is_group) {
+                data["group_id"] = conversation.id;
+            }
+            axios.post(route("message.store"), data);
 
-        setMessageSending(true);
+        }
+        const onSendClick = () => {
+            if (messageSending) {
+                return;
+            }
+            if (newMessage.trim() === "") {
+                setInputErrorMessages("Please enter a message or attach a file");
+                setTimeout(() => {
+                    setInputErrorMessages("");
+                }, 3000);
+                return;
+            }
 
-        axios.post(route("message.store"), formData)
-            .then(response => {
-                setNewMessage("");
-                setMessageSending(false);
-            })
-            .catch(error => {
-                setMessageSending(false);
-            });
-    };
+            const formData = new FormData();
+            formData.append("message", newMessage);
+            if (conversation.is_user) {
+                formData.append("receiver_id", conversation.id);
+            } else if (conversation.is_group) {
+                formData.append("group_id", conversation.id);
+            }
 
-    return (
-        <div className="flex flex-wrap items-start border-t border-slate-700 py-3">
-            <div className="order-2 flex-1 xs:flex-none xs:order-1 p-2">
-                <button className="p-1 text-gray-400 hover:text-gray-300 relative">
-                    <PaperClipIcon className="w-6"/>
-                    <input type="file" multiple
-                           className="absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer"/>
-                </button>
-                <button className="p-1 text-gray-400 hover:text-gray-300 relative">
-                    <PhotoIcon className="w-6"/>
-                    <input
-                        type="file" multiple accept="image/*"
-                        className="absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer"/>
-                </button>
-            </div>
-            <div className="order-1 px-3 xs:p-0 min-w-[220px] basis-full xs:basis-0 xs:order-2 flex-1 relative">
-                <div className="flex ">
-                    <NewMessageInput value={newMessage}
-                                     onSend={onSendClick}
-                                     onChange={(e) => setNewMessage(e.target.value)}
-                    />
-                    <button onClick={onSendClick}
-                            disabled={messageSending}
+            setMessageSending(true);
 
-                            className="btn btn-info rounded-l-none">
+            axios.post(route("message.store"), formData)
+                .then(response => {
+                    setNewMessage("");
+                    setMessageSending(false);
+                })
+                .catch(error => {
+                    setMessageSending(false);
+                });
+        };
 
-                        <PaperAirplaneIcon className="w-6"/>
-                        <span className="hidden sm:inline">Send</span>
+        return (
+            <div className="flex flex-wrap items-start border-t border-slate-700 py-3">
+                <div className="order-2 flex-1 xs:flex-none xs:order-1 p-2">
+                    <button className="p-1 text-gray-400 hover:text-gray-300 relative">
+                        <PaperClipIcon className="w-6"/>
+                        <input type="file" multiple
+                               className="absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer"/>
+                    </button>
+                    <button className="p-1 text-gray-400 hover:text-gray-300 relative">
+                        <PhotoIcon className="w-6"/>
+                        <input
+                            type="file" multiple accept="image/*"
+                            className="absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer"/>
                     </button>
                 </div>
-                {inputErrorMessages && (
-                    <p className="text-xs text-red-500">{inputErrorMessages}</p>
-                )}
+                <div className="order-1 px-3 xs:p-0 min-w-[220px] basis-full xs:basis-0 xs:order-2 flex-1 relative">
+                    <div className="flex ">
+                        <NewMessageInput value={newMessage}
+                                         onSend={onSendClick}
+                                         onChange={(e) => setNewMessage(e.target.value)}
+                        />
+                        <button onClick={onSendClick}
+                                disabled={messageSending}
+
+                                className="btn btn-info rounded-l-none">
+
+                            <PaperAirplaneIcon className="w-6"/>
+                            <span className="hidden sm:inline">Send</span>
+                        </button>
+                    </div>
+                    {inputErrorMessages && (
+                        <p className="text-xs text-red-500">{inputErrorMessages}</p>
+                    )}
+                </div>
+
+                <div className="order-3 xs:order-3 p-2 flex">
+                    <Popover className="relative">
+                        <Popover.Button className="p-1 text-gray-400 I hover : text-gray-300">
+                            <FaceSmileIcon className="w-6 h-6"/> </Popover.Button>
+                        <Popover.Panel className="absolute z-10 right-0 bottom-[100px]">
+                            <EmojiPicker theme="dark"
+                                         onEmojiClick={
+                                             (ev) => setNewMessage(
+                                                 newMessage + ev.emoji)
+                                         }>
+                            </EmojiPicker>
+                        </Popover.Panel>
+                    </Popover>
+
+                    <button onClick={onLikeClick} className="p-1 text-gray-400 hover:text-gray-300 ">
+                        <HandThumbUpIcon className="w-6"/>
+                    </button>
+                    <button onClick={onHeartClick} className="p-1 text-gray-400 hover:text-gray-300 ">
+                        <HeartIcon className="w-6"/>
+                    </button>
+                </div>
             </div>
-            <div className="order-3 xs:order-3 p-2 flex">
-                <button className="p-1 text-gray-400 hover:text-gray-300 ">
-                    <FaceSmileIcon className="w-6"/>
-                </button>
-                <button className="p-1 text-gray-400 hover:text-gray-300 ">
-                    <HandThumbUpIcon className="w-6"/>
-                </button>
-            </div>
-        </div>
-    );
-};
-export default MessageInput;
+        );
+    };
+    export default MessageInput;
 
